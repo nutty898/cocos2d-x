cfc0d5ac0044cf69924eb11c288e8d742ba9e74f
CaiWenzhi <wenzhi.cai@chukong-inc.com>
2014-04-25 18:11:58 +0800

    Add test scene

--- templates/cpp-template-csx/Classes/HelloWorldScene.cpp
+++ templates/cpp-template-csx/Classes/HelloWorldScene.cpp
@@ -1,5 +1,5 @@
 #include "HelloWorldScene.h"
-
+#include "../cocos2d/cocos/editor-support/cocostudio/CocoStudio.h"
 USING_NS_CC;
 
 Scene* HelloWorld::createScene()
@@ -9,10 +9,10 @@ Scene* HelloWorld::createScene()
     
     // 'layer' is an autorelease object
     auto layer = HelloWorld::create();
-
+    
     // add layer as a child to scene
     scene->addChild(layer);
-
+    
     // return the scene
     return scene;
 }
@@ -29,11 +29,11 @@ bool HelloWorld::init()
     
     Size visibleSize = Director::getInstance()->getVisibleSize();
     Point origin = Director::getInstance()->getVisibleOrigin();
-
+    
     /////////////////////////////
     // 2. add a menu item with "X" image, which is clicked to quit the program
     //    you may modify it.
-
+    
     // add a "close" icon to exit the progress. it's an autorelease object
     auto closeItem = MenuItemImage::create(
                                            "CloseNormal.png",
@@ -41,16 +41,16 @@ bool HelloWorld::init()
                                            CC_CALLBACK_1(HelloWorld::menuCloseCallback, this));
     
 	closeItem->setPosition(Point(origin.x + visibleSize.width - closeItem->getContentSize().width/2 ,
-                                origin.y + closeItem->getContentSize().height/2));
-
+                                 origin.y + closeItem->getContentSize().height/2));
+    
     // create menu, it's an autorelease object
     auto menu = Menu::create(closeItem, NULL);
     menu->setPosition(Point::ZERO);
     this->addChild(menu, 1);
-
+    
     /////////////////////////////
     // 3. add your codes below...
-
+    
     // add a label shows "Hello World"
     // create and initialize a label
     
@@ -58,25 +58,29 @@ bool HelloWorld::init()
     
     // position the label on the center of the screen
     label->setPosition(Point(origin.x + visibleSize.width/2,
-                            origin.y + visibleSize.height - label->getContentSize().height));
-
+                             origin.y + visibleSize.height - label->getContentSize().height));
+    
     // add the label as a child to this layer
     this->addChild(label, 1);
-
+    
     // add "HelloWorld" splash screen"
     auto sprite = Sprite::create("HelloWorld.png");
-
+    
     // position the sprite on the center of the screen
     sprite->setPosition(Point(visibleSize.width/2 + origin.x, visibleSize.height/2 + origin.y));
-
-    // add the sprite as a child to this layer
-    this->addChild(sprite, 0);
-    
-    sprite = Sprite::create("csxres/ss.png");
-    
     
     // add the sprite as a child to this layer
-    this->addChild(sprite, 0);
+    //    this->addChild(sprite, 0);
+    
+    //    sprite = Sprite::create("csxres/ss.png");
+    //
+    //
+    //    // add the sprite as a child to this layer
+    //    this->addChild(sprite, 0);
+    FileUtils::getInstance()->addSearchPath("csxres");
+    Node * res = cocostudio::timeline::NodeCache::getInstance()->createNode("csxres/MainScene.csxscene");
+    
+    addChild(res);
     
     return true;
 }
@@ -88,9 +92,9 @@ void HelloWorld::menuCloseCallback(Ref* pSender)
 	MessageBox("You pressed the close button. Windows Store Apps do not implement a close button.","Alert");
     return;
 #endif
-
+    
     Director::getInstance()->end();
-
+    
 #if (CC_TARGET_PLATFORM == CC_PLATFORM_IOS)
     exit(0);
 #endif
